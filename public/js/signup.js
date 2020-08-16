d2cabaeaa9a18b4675b4cf0f7fa7046065488335
hrachgalstyan <hrachgalstyann@gmail.com>
2020-08-16 15:24:18 +0400

    login

--- public/js/signup.js
+++ public/js/signup.js
@@ -8,7 +8,7 @@ export const signup = async (name, email, password, passwordConfirm) => {
   try {
     const res = await axios({
       method: 'POST',
-      url: 'http://127.0.0.1:5000/api/v1/users/signup',
+      url: 'https://baghadratomser.herokuapp.com/api/v1/users/signup',
       data: {
         name,
         email,
