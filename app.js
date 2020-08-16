4aa7d6f9e9f38d23268026fbb9a1f8b819819a07
hrachgalstyan <hrachgalstyann@gmail.com>
2020-08-16 16:21:37 +0400

    Access

--- app.js
+++ app.js
@@ -17,6 +17,12 @@ const viewRouter = require(`${__dirname}/routes/viewRoutes`);
 
 const app = express();
 
+app.use(function(req,res,next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+})
+
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
